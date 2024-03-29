88e968efc0e5834dee26c2586e74763312c0c0ce
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-03-29 12:43:54 +0100

    reset styles and adding react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryCLientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<QueryCLientProvider client={queryClient}>
+			<App />
+		</QueryCLientProvider>
+	</React.StrictMode>
+);
